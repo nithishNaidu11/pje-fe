944b42b1e3e142e66e3bb7d8b63ce3c7be2a1ce5
Urvashi Soni <35064805+misss-popcorn@users.noreply.github.com>
2022-12-21 17:08:43 +0530

    refactor

--- src/containers/PushNotification.tsx
+++ src/containers/PushNotification.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import OneSignal from 'react-onesignal';
 
 export const PushNotification = () => {
-    const appId = process.env.REACT_APP_ONE_SIGNAL_APP_ID;
+    const appId = process.env.REACT_APP_PUSH_NOTIFICATION_APP_ID;
 
     React.useEffect(() => {
         if (appId)
             OneSignal?.init({
                 appId
-            }).then(() => {
-                OneSignal.showSlidedownPrompt();
             });
         OneSignal.on('subscriptionChange', function (isSubscribed) {
             console.log("The user's subscription state is now:", isSubscribed);
         });
     }, []);
-    return <></>;
+    return (
+        <>
+            <div className="onesignal-customlink-container"></div>
+        </>
+    );
 };

b3558e52928c459e2df448fc94377baf7e140cca
Urvashi Soni <35064805+misss-popcorn@users.noreply.github.com>
2022-12-21 12:18:14 +0530

    feat: :zap: integrated app id

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import OneSignal from 'react-onesignal';
-
 import RoutesContainer from './routes';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -22,17 +20,6 @@ const theme = createTheme({
 const queryClient = new QueryClient();
 
 const App = () => {
-    React.useEffect(() => {
-        OneSignal?.init({
-            appId: '2c42941b-8210-4add-aa05-bf60a7c3c6a7'
-        }).then(() => {
-            OneSignal.showSlidedownPrompt();
-        });
-        OneSignal.on('subscriptionChange', function (isSubscribed) {
-            console.log("The user's subscription state is now:", isSubscribed);
-            OneSignal.setExternalUserId('urvashi');
-        });
-    }, []);
     return (
         <ThemeProvider theme={theme}>
             <QueryClientProvider client={queryClient}>

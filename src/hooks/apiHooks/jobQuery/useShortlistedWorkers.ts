2e6386b5f41477e4ed7c5f1b65952f0d995f313c
Urvashi Soni <35064805+misss-popcorn@users.noreply.github.com>
2023-01-12 11:51:52 +0530

    dev dummy commit

--- src/hooks/apiHooks/jobQuery/useShortlistedWorkers.ts
+++ src/hooks/apiHooks/jobQuery/useShortlistedWorkers.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { shortlistWorkers } from 'api/jobQuery';
-import { ApiError, ShortlistWorkerProps, Worker } from 'interfaces';
+import { type ApiError, ShortlistWorkerProps, Worker } from 'interfaces';
 
 type Params = {
     companyId: string;

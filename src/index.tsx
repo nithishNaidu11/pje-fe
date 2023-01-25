b4f59dde5942106a0e64eb2ae1904ec727c57138
Urvashi Soni <35064805+misss-popcorn@users.noreply.github.com>
2023-01-25 13:10:36 +0530

    add sentry to all env

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import { createRoot } from 'react-dom/client';
+import { ErrorTracker } from 'utils';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ErrorTracker.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_ENVIRONMENT,
+    release: process.env.REACT_APP_VERSION
+});
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -18,3 +25,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+window.addEventListener('unhandledrejection', event => {
+    ErrorTracker.captureException({
+        location: window.location.href,
+        reason: event.reason
+    });
+    event.preventDefault();
+});
